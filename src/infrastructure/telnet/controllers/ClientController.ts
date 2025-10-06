82e978b14e4976f4ca35ba50e3e1cfa950a6f9ec
Raphael Marandet <raphael.marandet@gmail.com>
2025-10-06 18:14:08 +0200

    telnet controller

--- src/infrastructure/telnet/controllers/ClientController.ts
+++ src/infrastructure/telnet/controllers/ClientController.ts
@@ -10,11 +10,14 @@ export class ClientController {
     }
     /**
      * One client is connecting to telnet server.
+     * The function parameter is the telnet socket
+     * We listen the events fired by this socket, and we execute the corresponding use cases.
      */
     connect(socket: TelnetClient) {
         const clientId = getUID();
-        // register this socket
         const pRequestCredentials = this.requestCredentials.bind(this);
+        // firsts messages are expected to be credentials
+        // login (first message), then password (second message)
         socket.on('data', pRequestCredentials);
     }
 
