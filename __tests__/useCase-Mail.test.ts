4b84e9f7b2e126704685951fa403ae56b5b2687e
Raphael Marandet <raphael.marandet@gmail.com>
2025-11-25 23:54:38 +0100

    unit testing mail messenging.

--- __tests__/useCase-Mail.test.ts
+++ __tests__/useCase-Mail.test.ts
@@ -1,10 +1,15 @@
 import { asClass, asValue, AwilixContainer, createContainer } from 'awilix';
 import { UIDGenerator } from '../src/infrastructure/services/UIDGenerator';
 import { User } from '../src/domain/entities/User';
+import { MailMessage } from '../src/domain/entities/MailMessage';
+import { MailInbox } from '../src/domain/entities/MailInbox';
+import { SendMailMessage } from '../src/application/use-cases/mail/SendMailMessage';
 
 describe('UseCase Mail', () => {
     const currentDate: Date = new Date();
     let container: AwilixContainer<any>;
+    let mailMessages: Map<string, MailMessage>;
+    let mailInboxes: Map<string, MailInbox>;
     beforeEach(() => {
         // mailInboxRepository
         // mailMessageRepository
@@ -12,23 +17,25 @@ describe('UseCase Mail', () => {
         // time
         // serverConfig
         // idGenerator
+        mailMessages = new Map<string, MailMessage>();
+        mailInboxes = new Map<string, MailInbox>();
         container = createContainer();
         container.register({
             time: asValue({
-                now: currentDate.getTime(),
-                renderDate: 'xx-xx-xx',
-                renderDuration: '',
-                convertToMilliseconds: 0,
+                now: () => currentDate.getTime(),
+                renderDate: () => 'xx-xx-xx',
+                renderDuration: () => '',
+                convertToMilliseconds: () => 0,
             }),
-            serverConfig: {
+            serverConfig: asValue({
                 getVariables: () => {
                     return {
                         mailMaxExpirationDays: 30,
                     };
                 },
-            },
-            idGenerator: asClass(UIDGenerator).singleton,
-            userRepository: {
+            }),
+            idGenerator: asClass(UIDGenerator).singleton(),
+            userRepository: asValue({
                 get: (id: string): User | undefined => {
                     if (id == '1') {
                         return {
@@ -67,8 +74,57 @@ describe('UseCase Mail', () => {
                         return undefined;
                     }
                 },
-            },
-            mailMessageRepository:
+            }),
+            mailMessageRepository: asValue({
+                save: async (message: MailMessage) => {
+                    mailMessages.set(message.id, message);
+                },
+            }),
+            mailInboxRepository: asValue({
+                save: async (mib: MailInbox) => {
+                    mailInboxes.set(mib.userId + '-' + mib.messageId, mib);
+                },
+                findByUserId: async (userId: string) => {
+                    return Array.from(mailInboxes.values()).filter(
+                        (mib: MailInbox) => mib.userId === userId
+                    );
+                },
+            }),
+            sendMailMessage: asClass(SendMailMessage).singleton(),
+        });
+    });
+    describe('sendMailMessage', () => {
+        it('should send message to user 2 & 3 from user 1', async () => {
+            const sendMailMessage = container.resolve<SendMailMessage>('sendMailMessage');
+            await sendMailMessage.execute(
+                '1',
+                ['2', '3'],
+                'dungeon raid tonight 21:00',
+                'please comme with your mightiest weapons.'
+            );
+            const mailSent = Array.from(mailMessages.values())[0];
+            expect(mailSent).toMatchObject({
+                content: 'please comme with your mightiest weapons.',
+                topic: 'dungeon raid tonight 21:00',
+                recipientIds: ['2', '3'],
+                senderId: '1',
+            });
+            const mailInbox0 = Array.from(mailInboxes.values())[0];
+            const mailInbox1 = Array.from(mailInboxes.values())[1];
+            expect(mailInbox0).toMatchObject({
+                userId: '2',
+                tag: 1,
+                deleted: false,
+                kept: false,
+                read: false,
+            });
+            expect(mailInbox1).toMatchObject({
+                userId: '3',
+                tag: 1,
+                deleted: false,
+                kept: false,
+                read: false,
+            });
         });
     });
 });
--- src/infrastructure/services/UIDGenerator.ts
+++ src/infrastructure/services/UIDGenerator.ts
@@ -1,12 +1,27 @@
 import { IIdGenerator } from '../../application/ports/services/IIdGenerator';
 import { generateUID } from '../../libs/uid-generator';
-import { getMinimalMissingValue } from 'o876-json-db/src/get-minimal-missing-value';
 
 export class UIDGenerator implements IIdGenerator {
     generateUID(): string {
         return generateUID(10);
     }
     getMinimalMissingValue(aValues: number[], minValue: number = 1): number {
-        return getMinimalMissingValue(aValues, minValue);
+        const aSortedValues = [
+            ...new Set(aValues.filter((x) => Number.isInteger(x) && x >= minValue)),
+        ].sort((a, b) => a - b);
+        if (aSortedValues.length == 0) {
+            return minValue;
+        }
+        if (minValue > aSortedValues[aSortedValues.length - 1]) {
+            return minValue;
+        }
+        let nExpectedValue = minValue;
+        for (let i = 0, l = aSortedValues.length; i < l; ++i) {
+            if (aSortedValues[i] > nExpectedValue) {
+                return nExpectedValue;
+            }
+            ++nExpectedValue;
+        }
+        return nExpectedValue;
     }
 }
