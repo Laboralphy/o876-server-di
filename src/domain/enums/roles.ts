82e55ed68b17d27a6b613ca12d3c1061b6969a99
Raphael Marandet <raphael.marandet@gmail.com>
2025-12-02 17:55:08 +0100

    adding user role service

--- src/domain/enums/roles.ts
+++ src/domain/enums/roles.ts
@@ -1,5 +1,6 @@
 export enum ROLES {
-    MODERATOR = 'MODERATOR',
-    GAME_MASTER = 'GAME_MASTER',
-    ADMIN = 'ADMIN',
+    MODERATOR = 'MODERATOR', // This role allows users to moderate other users' content and messages
+    GAME_MASTER = 'GAME_MASTER', // this role gives users some special power over the game
+    ADMIN = 'ADMIN', // This role gives users access to administration commands for server maintenance
+    SERVICE = 'SERVICE', // This role is used to automatize tasks, sending public email or announcement on chat channels
 }
